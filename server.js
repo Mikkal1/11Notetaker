9c697a86880c39e126d51a88a2e8c7c3ca3a06cf
Mikkal1 <mramcharitar2@gmail.com>
2024-02-04 18:21:52 -0500

    For submission.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// set the port based on environment (more on environments later)
+const port = process.env.PORT || 3000;
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + "/public"));
+
+const routes = require("./routes/router");
+app.use("/", routes);
+
+app.listen(port, function () {
+  console.log("Our app is running on http://localhost:" + port);
+});
